cf2c1c1225ceabdf94a6b10752edae59029182d7
rjnasers <rjnasers@iastate.edu>
2023-11-19 22:01:52 -0600

    Fixing strange lighting bug. That was a bitch to find but now I know more about where to find that one.

--- src/main/java/openblocks/client/renderer/tileentity/guide/TileEntityBuilderGuideRenderer.java
+++ src/main/java/openblocks/client/renderer/tileentity/guide/TileEntityBuilderGuideRenderer.java
@@ -118,7 +118,7 @@ public class TileEntityBuilderGuideRenderer extends TileEntityGuideRenderer {
         }
 
         GL11.glEnable(GL11.GL_TEXTURE_2D);
-        GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glEnable(GL11.GL_LIGHTING);
     }
 
     protected void drawCube(float theta, float phi) {
