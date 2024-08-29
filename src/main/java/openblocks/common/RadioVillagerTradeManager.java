bb73303f4f6a52e62e19a9ff3d22b74e6b401f38
Jakub <53441451+kuba6000@users.noreply.github.com>
2024-08-29 22:43:49 +0200

    Fix Radio villager trades in NEI (#22)
    * Fix Radio villager trades
    
    * Update dependencies.gradle

--- src/main/java/openblocks/common/RadioVillagerTradeManager.java
+++ src/main/java/openblocks/common/RadioVillagerTradeManager.java
@@ -1,6 +1,12 @@
 package openblocks.common;
 
+import static com.kuba6000.mobsinfo.api.VillagerTrade.create;
+import static com.kuba6000.mobsinfo.api.VillagerTrade.createItem;
+
+import java.util.ArrayList;
 import java.util.Random;
+
+import javax.annotation.Nonnull;
 
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.init.Blocks;
@@ -11,10 +17,15 @@ import net.minecraft.village.MerchantRecipe;
 import net.minecraft.village.MerchantRecipeList;
 import net.minecraftforge.oredict.OreDictionary;
 
+import com.kuba6000.mobsinfo.api.IVillagerInfoProvider;
+import com.kuba6000.mobsinfo.api.VillagerTrade;
+
+import cpw.mods.fml.common.Optional;
 import cpw.mods.fml.common.registry.VillagerRegistry.IVillageTradeHandler;
 import openblocks.Config;
 
-public class RadioVillagerTradeManager implements IVillageTradeHandler {
+@Optional.Interface(iface = "com.kuba6000.mobsinfo.api.IVillagerInfoProvider", modid = "mobsinfo")
+public class RadioVillagerTradeManager implements IVillageTradeHandler, IVillagerInfoProvider {
 
     private static ItemStack randomItemAmount(Random random, Item item, int min, int max) {
         int amount = random.nextInt(max - min) + min;
@@ -39,4 +50,24 @@ public class RadioVillagerTradeManager implements IVillageTradeHandler {
                 .addToListWithCheck(new MerchantRecipe(randomEmeralds(random, 3, 7), new ItemStack(Blocks.jukebox)));
     }
 
+    @Optional.Method(modid = "mobsinfo")
+    @Override
+    public void provideTrades(@Nonnull EntityVillager villager, int profession,
+            @Nonnull ArrayList<VillagerTrade> trades) {
+        if (Config.radioVillagerRecords) {
+            for (ItemStack record : OreDictionary.getOres("record")) {
+                trades.add(
+                        create(createItem(Items.emerald).withPossibleSizes(7, 15), createItem(record))
+                                .withChance(0.01d));
+            }
+        }
+        trades.add(
+                create(
+                        createItem(Items.emerald).withPossibleSizes(1, 2),
+                        createItem(Item.getItemFromBlock(Blocks.noteblock))).withChance(0.5d));
+        trades.add(
+                create(
+                        createItem(Items.emerald).withPossibleSizes(3, 7),
+                        createItem(Item.getItemFromBlock(Blocks.jukebox))).withChance(0.25d));
+    }
 }
